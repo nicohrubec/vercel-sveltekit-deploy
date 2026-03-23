c80ae84ab7cb1e6677289a2683f11d49502657d4
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-03-23 11:25:25 +0100

    .

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/sveltekit';
 Sentry.init({
   dsn: import.meta.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
+  debug: true,
 });
 
 export const handleError = Sentry.handleErrorWithSentry();
